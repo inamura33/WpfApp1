fe8cdd5c4cea333c63abd82dc3fa4807b7bccd7e
稲邑 <nyman71@hotmail.com>
2019-09-23 17:53:36 +0900

    Reposを空にしてからプロジェクトを投入

---
+++ WpfApp1/WpfApp1/MyGridView.xaml.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// MyGridView.xaml の相互作用ロジック
+    /// </summary>
+    public partial class MyGridView : UserControl
+    {
+        //private  int GRID_SIZE = 20;
+        public MyGridView()
+        {
+            InitializeComponent();
+        }
+
+        private void MyCanvas_Loaded(object sender, RoutedEventArgs e)
+        {
+
+            BuildView(20);
+        }
+
+        private void MyCanvas_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            BuildView(20);
+        }
+        // キャンバスに線を描画する
+        private void BuildView(int GRID_SIZE)
+        {
+            myCanvas.Children.Clear();
+
+
+            Style lineStyle = this.FindResource("GridLineStyle") as Style;
+
+            for (int i = 0; i < this.ActualWidth; i += GRID_SIZE)
+            {
+                Line line = new Line()
+                {
+                    X1 = i,
+                    Y1 = 0,
+                    X2 = i,
+                    Y2 = this.ActualHeight,
+                    Style = lineStyle
+                };
+
+                myCanvas.Children.Add(line);
+            }
+
+            for (int i = 0; i < this.ActualHeight; i += GRID_SIZE)
+            {
+                Line line = new Line()
+                {
+                    X1 = 0,
+                    Y1 = i,
+                    X2 = this.ActualWidth,
+                    Y2 = i,
+                    Style = lineStyle
+                };
+
+                myCanvas.Children.Add(line);
+            }
+        }
+
+        private void MyCanvas_MouseWheel(object sender, MouseWheelEventArgs e)
+        {
+
+        }
+
+        private void MyCanvas_MouseRightButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            //MessageBox.Show("ころころ");
+            //int delta = Math.Abs(e.Delta / 120);
+            System.Diagnostics.Debug.WriteLine("ぽち");
+            int delta = 10;
+            BuildView(20 + (delta * 2));
+        }
+    }
+}
